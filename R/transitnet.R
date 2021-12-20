a228d49539507b818bfccf701ebd2cda7cfdbb93
Luuk van der Meer <luukvandermeer@live.nl>
2021-12-20 17:44:58 +0100

    refactor: Remove unused dependencies :construction:

--- R/transitnet.R
+++ R/transitnet.R
@@ -23,64 +23,6 @@ import_transitnet = function(path, extent = NULL) {
     extent = sf::st_transform(extent, 4326)
     # Filter.
     G = filter_gtfs(G, space = extent)
-    # # Suppress no-visible binding notes.
-    # stop_id = trip_id = route_id = agency_id = service_id = shape_id = NULL
-    # from_stop_id = to_stop_id = NULL
-    # # Filter stops.
-    # # Only keep stops that intersect with the given spatial extent.
-    # stop_pts = sfheaders::sfc_point(G$stops[, c("stop_lon", "stop_lat")])
-    # sf::st_crs(stop_pts) = 4326
-    # keep_stops = G$stops[sf::st_intersects(extent, stop_pts)[[1]], ]$stop_id
-    # G$stops = subset(G$stops, stop_id %in% keep_stops)
-    # # Filter trips.
-    # # Only keep trips that contain filtered stops.
-    # keep_trips = unique(G$stop_times$trip_id)
-    # G$trips = subset(G$trips, trip_id %in% keep_trips)
-    # # Filter timetable.
-    # # Only keep exact stop times at filtered stops.
-    # # Timetables may be specified frequency based rather than with exact times.
-    # # In that case only keep frequencies belonging to filtered trips.
-    # G$stop_times = subset(G$stop_times, stop_id %in% keep_stops)
-    # if (! is.null(G$frequencies)) {
-    #   G$frequencies = subset(G$trips, trip_id %in% keep_trips)
-    # }
-    # # Filter routes.
-    # # Only keep routes that contain filtered trips.
-    # keep_routes = unique(G$trips$route_id)
-    # G$routes = subset(G$routes, route_id %in% keep_routes)
-    # # Filter agencies.
-    # # Only keep agencies that operate filtered routes.
-    # # Routes don't need to specify agency ids if there is only one agency.
-    # # In that case just keep all agencies.
-    # if (! is.null(G$routes$agency_id)) {
-    #   keep_agencies = unique(G$routes$agency_id)
-    #   G$agency = subset(G$agency, agency_id %in% keep_agencies)
-    # }
-    # # Filter services.
-    # # Only keep services that serve filtered trips.
-    # keep_services = unique(G$trips$service_id)
-    # if (! is.null(G$calendar)) {
-    #   G$calendar = subset(G$calendar, service_id %in% keep_services)
-    # }
-    # if (! is.null(G$calendar_dates)) {
-    #   G$calendar_dates = subset(G$calendar_dates, service_id %in% keep_services)
-    # }
-    # # Filter shapes.
-    # # Only keep shapes belonging to filtered trips.
-    # if (!is.null(G$shapes) && !is.null(G$trips$shape_id)) {
-    #   keep_shapes = unique(G$trips$shape_id)
-    #   G$shapes = subset(G$shapes, shape_id %in% keep_shapes)
-    # }
-    # # Filter transfers.
-    # # Only keep transfers between filtered stops.
-    # if (! is.null(G$transfers)) {
-    #   G$transfers = subset(G$transfers, from_stop_id %in% keep_stops & to_stop_id %in% keep_stops)
-    # }
-    # # Filter pathways.
-    # # Only keep pathways between filtered stops.
-    # if (! is.null(G$pathways)) {
-    #   G$pathways = subset(G$pathways, from_stop_id %in% keep_stops & to_stop_id %in% keep_stops)
-    # }
   }
   G
 }
